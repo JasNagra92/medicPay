4e9d1a727e8e64299578185c8b405535d0e91f16
jasmac <jaskarannagra@hotmail.com>
2024-02-26 10:23:29 -0800

    update gitignore

--- app.config.js
+++ app.config.js
@@ -30,13 +30,6 @@ export default {
     },
     plugins: ["expo-router"],
     extra: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
       eas: {
         projectId: "86931160-22a8-4971-8b8d-f5c975fc4e69",
       },
